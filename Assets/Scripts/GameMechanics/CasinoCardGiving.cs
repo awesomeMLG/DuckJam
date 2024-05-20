33a625363cd54a3f46a4e90133aef51cd76c2b0c
awesomeMLG <123847251+awesomeMLG@users.noreply.github.com>
2024-05-19 21:13:56 -0400

    updated and started on card variations

--- Assets/Scripts/GameMechanics/CasinoCardGiving.cs
+++ Assets/Scripts/GameMechanics/CasinoCardGiving.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CasinoCardGiving : MonoBehaviour
 {
     [SerializeField] private int numCardsToGive;
+
+    [SerializeField] private Text[] cardTextElements;
+
+    private string[] presetTypes = { "The Sun", "The Moon" };
+
+    private string[] unlockableTypes = { "The Fool", "The Magician", "The Hanged Man", "The Hierophant", "The Reverse", "The Clock", "The Ace of Spades"};
 
     private int[] cardsGiven;
 
